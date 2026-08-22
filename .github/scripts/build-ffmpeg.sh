281abacab6fedd2457e25ee8e44809bf1c1e4151
Rasmus Stisen <rasmusstisen@pm.me>
2026-08-22 14:01:36 +0200

    Revert GitHub username transfer back to rs-jensen
    The account is now rs-jensen again. Restores copyright headers and
    the Jellyfin plugin link that were changed to r-stisen in 11f6696.
    GHCR image tags stay on ghcr.io/schwponaco-org since packages are
    published under the org, not a personal account.

--- .github/scripts/build-ffmpeg.sh
+++ .github/scripts/build-ffmpeg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # LAPSE - Language-Agnostic subtitle synchronization engine
-# Copyright (C) 2026 Rasmus Stisen Jensen (r-stisen)
+# Copyright (C) 2026 Rasmus Stisen Jensen (rs-jensen)
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
